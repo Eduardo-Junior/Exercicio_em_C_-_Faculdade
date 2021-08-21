07a54ad614ecc13f5198ad8686bb62460462bf95
Eduardo <45540161+Eduardo-Junior@users.noreply.github.com>
2021-08-20 22:46:44 -0300

    Add files via upload

--- 12--08 - Revisão.c
+++ 12--08 - Revisão.c
@@ -1,6 +1,7 @@
-#include "stdlib.h"
-#include "stdio.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
 
 struct Pessoa{
 	char nome[100];
@@ -21,66 +22,165 @@ struct TCC{
 	
 }typedef TCC;
 	
-	TCC* inicio = NULL;
-	TCC* fim = NULL;
+TCC* inicio = NULL;
+TCC* fim = NULL;
 	
-	
-void cadastra(TCC *novo){
+void Cadastra(TCC *novo){
 	if (inicio == NULL){
-		inicio = (TCC*)malloc(sizeof(TCC));
-		
-		if (inicio != NULL){
-			inicio = novo;
-			fim = inicio;
-		}
+		inicio = novo;
 		
 	}else{
-		fim->proximo = (TCC*)malloc(sizeof(TCC));
 		fim->proximo = novo;
-		
-		fim = novo;
-		
 	}
+	
+	fim = novo;
 }
 
 
-void Listar(TTC* aux){
+void Registrar(){
+	
+	int c;
+	TCC* novo = (TCC*)malloc(sizeof(TCC));
+	
+	_flushall();
+	printf("Titulo do TCC: ");
+	fgets(novo->titulo, 99, stdin);
+	
+	_flushall();
+	printf("Curso do TCC: ");
+	fgets(novo->curso, 99, stdin);
+	
+	_flushall();
+	printf("Ano de conclusão: ");
+	scanf("%d", &novo->ano);
+	
+	_flushall();
+	printf("Quantidade de participantes: ");
+	scanf("%d", &novo->qtdAlunos);
+	
+	_flushall();
+	printf("Orientador: ");
+	fgets(novo->professor.nome, 99, stdin);
+	
+	_flushall();
+	printf("Email do orientador: ");
+	fgets(novo->professor.email, 99, stdin);
+	
+	
+	novo->aluno = (Pessoa*)malloc(sizeof(Pessoa)*novo->qtdAlunos);
+	
+	for(c = 0; c<novo->qtdAlunos; c++){
+		
+		_flushall();
+		printf("Aluno %d: ", c+1);
+		fgets(novo->aluno[c].nome, 99, stdin);
+		
+		_flushall();
+		printf("Email do aluno %d: ", c+1);
+		fgets(novo->aluno[c].email, 99, stdin);
+		
+	}
+	
+	
+	novo->proximo = NULL;
+	
+	Cadastra(novo);
+}
+
+
+void Listar(TCC* aux){
 	
 	int i = 0;
-	TCC* aux = inicio;
+//	aux = inicio;
 	
 	
 	if (aux != NULL){
-		printf("TCC - Titulo: %s", aux->titulo);
+		printf("===============TCC===============");
+		printf("\n\nTCC - Titulo: %s", aux->titulo);
 		printf("TCC - Curso: %s", aux->curso);
 		printf("TCC - Ano: %d", aux->ano);
-		printf("TCC - Quantidade de participantes: %d", aux->qtdAlunos);
+		printf("\nTCC - Quantidade de participantes: %d\n\n", aux->qtdAlunos);
 		
 		printf("Professor - nome: %s", aux->professor.nome);
-		printf("Professor - email %s", aux->professor.email);
+		printf("Professor - email: %s\n\n", aux->professor.email);
 		
 		
-		for(i = 0; i<aux->qtdAlunos; i++){
+		for(i = 0; i < aux->qtdAlunos; i++){
 			
-			printf("Aluno - nome: %s", aux->aluno[i].email);
-			printf("Aluno - email %s", aux->aluno[i].nome);
+			printf("Aluno - nome: %s", aux->aluno[i].nome);
+			printf("Aluno - email: %s\n\n", aux->aluno[i].email);
 			
 		}
 		
 		aux = aux->proximo;
+        Listar(aux);
 	}
 	
-	imprime(aux);
+	   
+}
+
+void ListarOrientador(TCC* aux){
+	
+	int i = 0;
+//	aux = inicio;
+	
+	
+	if (aux != NULL){
+		if(){
+			
+		}
+		printf("===============TCC===============");
+		printf("\n\nTCC - Titulo: %s", aux->titulo);
+		printf("TCC - Curso: %s", aux->curso);
+		printf("TCC - Ano: %d", aux->ano);
+		printf("\nTCC - Quantidade de participantes: %d\n\n", aux->qtdAlunos);
+		
+		printf("Professor - nome: %s", aux->professor.nome);
+		printf("Professor - email: %s\n\n", aux->professor.email);
+		
+		
+		for(i = 0; i < aux->qtdAlunos; i++){
+			
+			printf("Aluno - nome: %s", aux->aluno[i].nome);
+			printf("Aluno - email: %s\n\n", aux->aluno[i].email);
+			
+		}
+		
+		aux = aux->proximo;
+        Listar(aux);
+	}
+	
+	   
 }
 
 int main(){
 	
-	printf("Digite o título do TCC: ");
-	scanf()
+	setlocale(LC_ALL,"Portuguese");
+	int op;
 	
-	cadastra();
-	
-	imprime();
+	do{
+		printf("1 - Cadastrar\n");
+		printf("2 - Exibir dados cadastrados\n");
+		printf("0 - Sair\n");
+		
+		printf("Digite a opção desejada: ");
+		scanf("%d", &op);
+		
+		switch(op){
+			case 1:
+				Registrar();
+				break;
+			
+			case 2:
+				Listar(inicio);
+				break;
+				
+			default:
+				printf("Opção não existente. Tente novamente");
+				break;
+		}
+		
+	}while(op != 0);
 	
 	return 0;
 }
